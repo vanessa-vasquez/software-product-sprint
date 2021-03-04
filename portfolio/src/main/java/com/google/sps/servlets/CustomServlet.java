1b535558d0fc9dcfbb45714b35c9f294d68e881d
Vanessa <vvasquez528@gmail.com>
2021-03-04 23:41:40 +0000

    Modify servlet so doGet returns JSON string

--- portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
@@ -1,25 +1,35 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
 
 /* Handles requests sent to the /custom-servlet URL. */
 @WebServlet("/custom-servlet")
 public class CustomServlet extends HttpServlet {
 
-  ArrayList<String> messages; 
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    ArrayList<String> messages = new ArrayList<String>();  
     messages.add("Hello");
     messages.add("from");
     messages.add("Nokia Phone");
-    
+
+    GsonBuilder gsonBuilder = new GsonBuilder();
+
+    Gson gson = gsonBuilder.create();
+
+    String JSONString = gson.toJson(messages);
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>This is my custom servlet.</h1>");
+    response.getWriter().println(JSONString);
   }
 }
