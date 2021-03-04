3fdaf7ef97d5e5bc9bcc75949139c92d56108be0
Vanessa <vvasquez528@gmail.com>
2021-03-04 18:43:48 +0000

    Incorporate fetch() into portfolio code
    - Add new servlet to portfolio
    - Fix file comments
    - Fix git errors

--- portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
@@ -6,11 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
 /* Handles requests sent to the /custom-servlet URL. */
-=======
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
->>>>>>> 2223be4fa3f5dc35ffc34ac5e815b016f31f3e86
+
 @WebServlet("/custom-servlet")
 public class CustomServlet extends HttpServlet {
 
