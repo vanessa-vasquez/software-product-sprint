8fe19a9552d072f2283c3de2d4d323606941d6a5
Vanessa <vvasquez528@gmail.com>
2021-03-04 23:03:45 +0000

    Modify servlet to contain a list of messages

--- portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CustomServlet.java
@@ -5,14 +5,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /* Handles requests sent to the /custom-servlet URL. */
-
 @WebServlet("/custom-servlet")
 public class CustomServlet extends HttpServlet {
 
+  ArrayList<String> messages; 
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    messages.add("Hello");
+    messages.add("from");
+    messages.add("Nokia Phone");
+    
     response.setContentType("text/html;");
     response.getWriter().println("<h1>This is my custom servlet.</h1>");
   }
